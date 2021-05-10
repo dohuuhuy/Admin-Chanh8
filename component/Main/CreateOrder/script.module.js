5995ff1c21f8dcb516f9934014b270f5234328b3
Do Huu Huy <dohuuhuy2@gmail.com>
2021-05-10 20:41:12 +0700

    okey

--- component/Main/CreateOrder/script.module.js
+++ component/Main/CreateOrder/script.module.js
@@ -1,87 +1,43 @@
 $(document).ready(function () {
-  var current_fs, next_fs, previous_fs; //fieldsets
-  var opacity;
-  var current = 1;
-  var steps = $("fieldset").length;
+  var navListItems = $("ul.setup-panel li a"),
+    allWells = $(".setup-content"),
+    allNextBtn = $(".nextBtn");
+  allWells.hide();
 
-  var numList = $("#progressbar li").size();
-  console.log(`numList`, numList);
-  var withList = $("#progressbar").width();
-  console.log(`withList`, withList);
-  var evenWidth = (1 / numList) * 100;
-  console.log(`evenWidth`, evenWidth);
-  $("#progressbar li").css("width", evenWidth + "%");
+  navListItems.click(function (e) {
+    e.preventDefault();
+    var $target = $($(this).attr("href")),
+      $item = $(this);
 
-  setProgressBar(current);
+    if (!$item.hasClass("disabled")) {
+      navListItems.removeClass("btn-info").addClass("btn-success");
+      $item.addClass("btn-info");
+      allWells.hide();
+      $target.show();
+      $target.find("input:eq(0)").focus();
+    }
+  });
 
-  $(".next").click(function () {
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
+  allNextBtn.click(function () {
+    var curStep = $(this).closest(".setup-content"),
+      curStepBtn = curStep.attr("id"),
+      nextStepWizard = $('ul.setup-panel li a[href="#' + curStepBtn + '"]')
+        .parent()
+        .next()
+        .children("a"),
+      curInputs = curStep.find("input[type='text'],input[type='url']"),
+      isValid = true;
 
-    //Add Class Active
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate(
-      { opacity: 0 },
-      {
-        step: function (now) {
-          // for making fielset appear animation
-          opacity = 1 - now;
-
-          current_fs.css({
-            display: "none",
-            position: "relative",
-          });
-          next_fs.css({ opacity: opacity });
-        },
-        duration: 500,
+    $(".form-group").removeClass("has-error");
+    for (var i = 0; i < curInputs.length; i++) {
+      if (!curInputs[i].validity.valid) {
+        isValid = false;
+        $(curInputs[i]).closest(".form-group").addClass("has-error");
       }
-    );
-    setProgressBar(++current);
+    }
+
+    if (isValid) nextStepWizard.removeAttr("disabled").trigger("click");
   });
 
-  $(".previous").click(function () {
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-
-    //Remove class active
-    $("#progressbar li")
-      .eq($("fieldset").index(current_fs))
-      .removeClass("active");
-
-    //show the previous fieldset
-    previous_fs.show();
-
-    //hide the current fieldset with style
-    current_fs.animate(
-      { opacity: 0 },
-      {
-        step: function (now) {
-          // for making fielset appear animation
-          opacity = 1 - now;
-
-          current_fs.css({
-            display: "none",
-            position: "relative",
-          });
-          previous_fs.css({ opacity: opacity });
-        },
-        duration: 500,
-      }
-    );
-    setProgressBar(--current);
-  });
-
-  function setProgressBar(curStep) {
-    var percent = parseFloat(100 / steps) * curStep;
-    percent = percent.toFixed();
-    $(".progress-bar").css("width", percent + "%");
-  }
-
-  $(".submit").click(function () {
-    return false;
-  });
+  $("ul.setup-panel li a.btn-success").trigger("click");
 });
